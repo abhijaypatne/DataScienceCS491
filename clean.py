93915893d497ff12b892a3af6f23f0e06fe4bccc
Abhijay Patne <abhijaypatne@gmail.com>
2016-10-06 04:38:22 -0500

    major changes
    clean.py:  correctCourses(), processCourses() and writeToFile() methods
    added
    big.txt: dictionary of words

--- clean.py
+++ clean.py
@@ -1,7 +1,55 @@
 from sys import argv;
 from pprint import pprint;
 import collections;
+import re
+from collections import Counter
 
+
+#def words(text): return re.findall(r'\w+', text.lower())
+
+def words(text): return re.findall(r'\w+', text)
+
+WORDS = Counter(words(open("A:\\new_Sync\\Box Sync\\academics\\sem3\\491\\assignments\\HW2\\big.txt").read()))
+
+def P(word, N=sum(WORDS.values())):
+    "Probability of `word`."
+    return WORDS[word] / N
+
+def correction(word):
+    "Most probable spelling correction for word."
+    return max(candidates(word), key=P)
+
+def candidates(word):
+    "Generate possible spelling corrections for word."
+    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+
+def known(words):
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edits2(word):
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+
+"""
+input: dictionary of corrected courses and professors, output file
+"""
+def writeToFile(dictProfCourses, outputFile):
+    file = open(outputFile, "w");
+    for key, value in sorted(dictProfCourse.iteritems()):
+        file.write(key + " - " + value + "\n");
+    file.close();
 
 """
 input: prof - course dictionary
@@ -11,6 +59,7 @@ def sortDict(dictProfCourse):
     sortedDictProfCourse = {};
     for key in sorted(dictProfCourse):
         sortedDictProfCourse[key] = dictProfCourse.get(key);
+        #print (key) + sortedDictProfCourse.get(key);
     return sortedDictProfCourse;
 
 """
@@ -53,6 +102,36 @@ def parseCatalog(listOfLines):
     return dictProfCourse;
 
 """
+input: dictionary of professors and courses
+output: same dictionary with corrected course names
+"""
+def correctCourses(dictProfCourse):
+    for key, value in dictProfCourse.iteritems():
+        dictProfCourse[key] = processCourses(value);
+    return dictProfCourse;
+
+"""
+input: string of dirty courses
+return : string of cleaned courses
+"""
+def processCourses(courses):
+    courseList = courses.split("|");
+    correctedCourses = "";
+    for course in courseList:
+        correctedCourse = "";
+        tokenList = re.split('(\W)', course);
+        for token in tokenList:
+            if (token == "&"):
+                token = "and";
+            elif (token.isalpha()):
+                token = correction(token);
+            else:
+                token = token;
+            correctedCourse += token;
+        correctedCourses += correctedCourse + "|";
+    return correctedCourses[:-1];
+
+"""
 read the input catalog file
 input: file name
 return: list of lines read
@@ -71,7 +150,18 @@ if __name__ == '__main__':
     listOfLines = readFile(classFile);
     dictProfCourse = parseCatalog(listOfLines);
     sortedDictProfCourse = sortDict(dictProfCourse);
+    sortedDictProfCourse;
+    #print sortedDictProfCourse;
+    dictProfCourse = correctCourses(dictProfCourse);
+    writeToFile(dictProfCourse, cleanClassFile);
+    #pprint(dictProfCourse);
+    #print (processCourses("parallel computation: models, algorithms, limits|Filtering & Prediction of Hidden Markov Models|parallel computation: modelz, algorithms, limits"));
 
     # this needs to be changed, as we have write in the same format as the input class.txt
-    with open(cleanClassFile, "w") as fout:
-        pprint(sortedDictProfCourse, fout)
+    #with open(cleanClassFile, "w") as fout:
+        #pprint(sortedDictProfCourse, fout)
+
+
+
+
+
