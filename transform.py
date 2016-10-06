4c6605dcd3d799894bb746c48c7184e1020534ad
Abhijay Patne <abhijaypatne@gmail.com>
2016-10-05 22:49:10 -0500

    writeData() completed

--- transform.py
+++ transform.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals;
 import csv;
 from urllib2 import urlopen;
 from bs4 import BeautifulSoup as BS;
@@ -17,11 +18,38 @@ def writeHeader(outputFile, header):
 def writeData(outputFile, soup):
     tables = soup.find_all("table", { "class" : "wikitable sortable" });
     reqTable = tables[1];
-    for row in reqTable.find_all("tr")[1:]:
-        columns = row.find_all("td");
-        number = columns[0].find('a').get_text().strip();
-        print number
 
+    file = open(outputFile, "a");
+    csvwriter = csv.writer(file);
+    count = 0;
+
+    for row in reqTable.find_all("tr")[1:51]:
+        output = []
+        columns = row.find_all("td");
+        # find game number
+        number = columns[0].find('a').get_text().strip();
+        output.append(number.encode('ascii', 'ignore'));
+
+        # find game year
+        year = columns[1].findAll("span")[-1].get_text()[-4:];
+        output.append(year.encode('ascii', 'ignore'));
+
+        # find winning team
+        winner = columns[2].find('a').get_text().strip();
+        output.append(winner);
+
+        # find score
+        score = columns[3].findAll("span")[-1].get_text();
+        output.append(score.encode('ascii', 'ignore'));
+
+        # find losing team
+        output.append(columns[4].find('a').get_text().strip().encode('ascii', 'ignore'));
+
+        # find venue
+        output.append(columns[5].findAll("span")[-1].get_text().strip().encode('ascii', 'ignore'));
+
+        csvwriter.writerow(output);
+        count += 1;
 
 
 # # starting point of the program
