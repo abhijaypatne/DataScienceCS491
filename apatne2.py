bc92b88886c68315ea3261b6b447f6a64d23d25e
Abhijay Patne <abhijaypatne@gmail.com>
2016-10-25 03:35:39 -0500

    initial commit of apatne2.py
    read the tweets from the output json file and create a list of tweets

--- apatne2.py
+++ apatne2.py
@@ -1,3 +1,26 @@
 import json;
+from pprint import pprint;
+
+"""
+read the input tweets file
+input: file name
+return: list of text tweets read
+"""
+def readFile(tweetsFile):
+    fileHandle = open(tweetsFile, "r");
+    data = json.load(fileHandle);
+    fileHandle.close();
+    length = len(data["statuses"]);
+    tweets = [];
+    for i in range(length):
+        tweets.append(data["statuses"][i]["text"]);
+    #print((data["statuses"][0]["text"]));
+    return tweets;
 
 
+if __name__ == "__main__":
+    print ("start");
+    tweetsFile = "smallsearch_output.txt";
+    tweets = readFile(tweetsFile);
+    print (type(tweets));
+    print (tweets);
