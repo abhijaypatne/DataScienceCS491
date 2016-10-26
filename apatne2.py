59a999766174541b05977de11bf2f9f25f1eb176
Abhijay Patne <abhijaypatne@gmail.com>
2016-10-26 02:51:02 -0500

    KMeansClustering
    - added code for creating n clusters using KMeansClustering
    - code prints top terms per cluster
    - initial version, need to check with tuning different parameters
    - find a way to visualize clusters and verify them

--- apatne2.py
+++ apatne2.py
@@ -1,5 +1,8 @@
 import json;
 from pprint import pprint;
+from sklearn.feature_extraction.text import TfidfVectorizer as tfidf;
+from sklearn.cluster import KMeans, MiniBatchKMeans
+
 
 """
 read the input tweets file
@@ -17,10 +20,30 @@ def readFile(tweetsFile):
     #print((data["statuses"][0]["text"]));
     return tweets;
 
+"""
+create a cluster of similar tweets
+input: list of tweets and number of clusters
+return: nothing, just print the top terms per cluster
+"""
+def kMeansClustering(tweets, number):
+    vectorizer = tfidf(max_df=0.6, min_df=0.05, stop_words='english');
+    x = vectorizer.fit_transform(tweets);
+    km = KMeans(n_clusters=5, init='k-means++', max_iter=100);
+    km.fit(x);
+    print("Top terms per cluster:");
+    order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+    terms = vectorizer.get_feature_names()
+    for i in range(5):
+        print("Cluster %d:" % i, end='')
+        for ind in order_centroids[i, :10]:
+            print(' %s' % terms[ind], end='')
+        print()
+
 
 if __name__ == "__main__":
     print ("start");
     tweetsFile = "smallsearch_output.txt";
     tweets = readFile(tweetsFile);
     print (type(tweets));
-    print (tweets);
+    kMeansClustering(tweets, 5);
+    #pprint (tweets);
