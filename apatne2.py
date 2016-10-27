83794a4e11a5e2a9d0a0714d24ece057e3c71bca
Abhijay Patne <abhijaypatne@gmail.com>
2016-10-26 21:58:46 -0500

    added two more vectorizers
    - found top n tweets using count and hash vectorizers

--- apatne2.py
+++ apatne2.py
@@ -1,8 +1,11 @@
 import json;
 from pprint import pprint;
-from sklearn.feature_extraction.text import TfidfVectorizer as tfidf;
+from sklearn.feature_extraction.text import TfidfVectorizer as tfidfVect;
+from sklearn.feature_extraction.text import CountVectorizer as cntVect;
+from sklearn.feature_extraction.text import HashingVectorizer as hashVect
 from sklearn.cluster import KMeans;
 from sklearn.metrics.pairwise import linear_kernel as lkernel;
+
 
 """
 read the input tweets file
@@ -25,8 +28,8 @@ create a cluster of similar tweets
 input: list of tweets and number of clusters
 return: nothing, just print the top terms per cluster
 """
-def kMeansClustering(tweets, number):
-    vectorizer = tfidf(max_df=0.6, min_df=0.05, stop_words='english');
+def findCentroidTweets(tweets, number):
+    vectorizer = tfidfVect(max_df=0.6, min_df=0.05, stop_words='english');
     x = vectorizer.fit_transform(tweets);
     km = KMeans(n_clusters=5, init='k-means++', max_iter=100);
     km.fit(x);
@@ -34,14 +37,11 @@ def kMeansClustering(tweets, number):
     order_centroids = km.cluster_centers_.argsort()[:, ::-1]
     terms = vectorizer.get_feature_names();
     centroidTweets = [[] for _ in range(number)]
-    for i in range(5):
-        print("Cluster %d:" % i, end='')
+    for i in range(number):
         for ind in order_centroids[i, :number]:
             centroidTweets[i].append(terms[ind]);
-            print(' %s' % terms[ind], end='')
         print()
-
-    pprint (centroidTweets);
+    return centroidTweets;
 
 """
 Purpose: find top n similar tweets to the query
@@ -52,18 +52,62 @@ input:
 
 output: list of top n similar tweets
 """
-def findTopNSimilarTweets(tweets, query, n):
+def findTopNSimilarTweetsTFIDF(tweets, query, n):
     queryList = [query];
     totalList = queryList + tweets;
-    vectorizer = tfidf(max_df=0.75, min_df=0.05, stop_words='english');
+    # create the vectorizer
+    vectorizer = tfidfVect(max_df=0.75, min_df=0.05, stop_words='english');
+    # transform the list of tweets into a sparse matrix which represents tf idf matrix
     tfidfMatrix = vectorizer.fit_transform(totalList);
+    # calculate cosine similarities of first document (Search term in this case)
+    # with all other documents
     cosSimilarities = lkernel(tfidfMatrix[0:1], tfidfMatrix).flatten();
+    # find top n matches by sorting cosine similarities and selecting last n items
     topNMatches = cosSimilarities.argsort()[:-(n+2):-1];
     topNSimilarTweets = [];
     for i in topNMatches[1:]:
         topNSimilarTweets.append(tweets[i-1]);
     return topNSimilarTweets;
 
+"""
+Same method using count vectorizer
+"""
+def findTopNSimilarTweetsCountVect(tweets, query, n):
+    queryList = [query];
+    totalList = queryList + tweets;
+    # create the vectorizer
+    vectorizer = cntVect(max_df=0.75, min_df=0.05, stop_words='english');
+    # transform the list of tweets into a sparse matrix which represents tf idf matrix
+    tfidfMatrix = vectorizer.fit_transform(totalList);
+    # calculate cosine similarities of first document (Search term in this case)
+    # with all other documents
+    cosSimilarities = lkernel(tfidfMatrix[0:1], tfidfMatrix).flatten();
+    # find top n matches by sorting cosine similarities and selecting last n items
+    topNMatches = cosSimilarities.argsort()[:-(n+2):-1];
+    topNSimilarTweets = [];
+    for i in topNMatches[1:]:
+        topNSimilarTweets.append(tweets[i-1]);
+    return topNSimilarTweets;
+
+"""
+Same method using hashing vectorizer
+"""
+def findTopNSimilarTweetsHashVect(tweets, query, n):
+    queryList = [query];
+    totalList = queryList + tweets;
+    # create the vectorizer
+    vectorizer = hashVect(stop_words='english');
+    # transform the list of tweets into a sparse matrix which represents tf idf matrix
+    tfidfMatrix = vectorizer.fit_transform(totalList);
+    # calculate cosine similarities of first document (Search term in this case)
+    # with all other documents
+    cosSimilarities = lkernel(tfidfMatrix[0:1], tfidfMatrix).flatten();
+    # find top n matches by sorting cosine similarities and selecting last n items
+    topNMatches = cosSimilarities.argsort()[:-(n+2):-1];
+    topNSimilarTweets = [];
+    for i in topNMatches[1:]:
+        topNSimilarTweets.append(tweets[i-1]);
+    return topNSimilarTweets;
 
 if __name__ == "__main__":
     print ("start");
@@ -72,9 +116,17 @@ if __name__ == "__main__":
     tweets = readFile(tweetsFile);
     print (type(tweets));
     # Query 1
+    n = 10;
     searchQuery = "donald trump";
-    topNSimilarTweets = findTopNSimilarTweets(tweets, searchQuery, 5);
-    #pprint(topNSimilarTweets);
+    topNSimilarTweetsTFIDF = findTopNSimilarTweetsTFIDF(tweets, searchQuery, n);
+    topNSimilarTweetsCount = findTopNSimilarTweetsCountVect(tweets, searchQuery, n);
+    topNSimilarTweetsHash = findTopNSimilarTweetsHashVect(tweets, searchQuery, n);
+    print ("Top ", n, " similar tweets using TFIDF Vectorizer : ");
+    print(topNSimilarTweetsTFIDF);
+    print ("Top ", n, " similar tweets using Count Vectorizer : ");
+    print(topNSimilarTweetsCount);
+    print ("Top ", n, " similar tweets using Hash Vectorizer : ");
+    print(topNSimilarTweetsHash);
     # Query 2
-    kMeansClustering(tweets, 5);
+    centroidTweets = findCentroidTweets(tweets, 5);
     #pprint (tweets);
